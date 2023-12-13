03e2a8a248f653d9264ca7740663e9ccbebf2f3a
haappyhj <149385733+haappyhj@users.noreply.github.com>
2023-12-13 21:50:31 +0900

    Add files via upload

---
+++ 2023_12_C.cpp
@@ -0,0 +1,15 @@
+﻿//standard input & output
+
+#include<stdio.h>
+// 메모하는 곳(#으로 쓸 경우 주석문)
+
+// main entry-point
+int main() {
+
+	printf("안녕하세요 박현지입니다.\n");
+	printf("개발하는 이유: 아직은 모르겠습니다\n");
+	printf("능력있는 개발자가 되고 싶어요");
+	
+
+	return 0;
+}
